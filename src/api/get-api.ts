bc90f2be1d90454f64696973ad7d07948cdd2c38
shion <shionlk98@gmail.com>
2025-01-28 19:47:08 -0300

    remove old api

--- src/api/get-api.ts
+++
@@ -1,9 +0,0 @@
-import productData from "@/data/products-data.json";
-
-
-export const getProductData = () => {
-  if (!productData) {
-    throw new Error("product data not found");
-  }
-  return productData;
-};
